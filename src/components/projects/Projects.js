e0cf293c76404139365a586ea5a24dd971bbd22b
Jody Lorist <66092262+jody29@users.noreply.github.com>
2021-12-29 13:22:54 +0100

    projects modified

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,11 +2,13 @@ import './projects.css'
 
 const Projects = () => {
     return (
-        <div className="projects">
+        <div className="projects" id='projects'>
             <h2>Mijn projecten</h2>
             <ul>
                 <li>
-                    <div></div>
+                    <div>
+                        <img src='/musicMatch.png' alt='musicMatch'></img>
+                    </div>
                     <h3>MusicMatch</h3>
                     <h4>Front-end &amp; Back-end</h4>
                 </li>
