03e8ab714ecc9cd7ce981f7e1822a12bcd16a15a
Jody Lorist <66092262+jody29@users.noreply.github.com>
2021-12-29 02:49:36 +0100

    projects added

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -5,7 +5,26 @@ const Projects = () => {
         <div className="projects">
             <h2>Mijn projecten</h2>
             <ul>
-
+                <li>
+                    <div></div>
+                    <h3>MusicMatch</h3>
+                    <h4>Front-end &amp; Back-end</h4>
+                </li>
+                <li>
+                    <div></div>
+                    <h3>Technoleon</h3>
+                    <h4>Front-end</h4>
+                </li>
+                <li>
+                    <div></div>
+                    <h3>Technoleon</h3>
+                    <h4>Front-end</h4>
+                </li>
+                <li>
+                    <div></div>
+                    <h3>Technoleon</h3>
+                    <h4>Front-end</h4>
+                </li>
             </ul>
         </div>
     )
